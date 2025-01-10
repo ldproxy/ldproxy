5cb94c81fffd737cfc5c5540b8a2785ba098ed98
Andreas Zahnen <zahnen@interactive-instruments.de>
2025-01-10 10:49:19 +0100

    fix ldproxy-cfg

--- ldproxy-cfg/src/main/java/de/ii/ldproxy/cfg/RequiredIncludes.java
+++ ldproxy-cfg/src/main/java/de/ii/ldproxy/cfg/RequiredIncludes.java
@@ -10,14 +10,23 @@ package de.ii.ldproxy.cfg;
 import com.fasterxml.jackson.databind.ext.Java7HandlersImpl;
 import java.util.List;
 import org.apache.hc.core5.http.NameValuePair;
+import schemacrawler.server.postgresql.PostgreSQLDatabaseConnector;
+import schemacrawler.tools.catalogloader.SchemaCrawlerCatalogLoader;
+import schemacrawler.tools.sqlite.SQLiteDatabaseConnector;
 
 class RequiredIncludes {
 
   private final Java7HandlersImpl java7Handlers;
   private final List<NameValuePair> nameValuePairs;
+  private final SchemaCrawlerCatalogLoader schemaCrawlerCatalogLoader;
+  private final PostgreSQLDatabaseConnector postgresSQLDatabaseConnector;
+  private final SQLiteDatabaseConnector sqLiteDatabaseConnector;
 
   RequiredIncludes() {
     this.java7Handlers = new Java7HandlersImpl();
     this.nameValuePairs = List.of();
+    this.schemaCrawlerCatalogLoader = new SchemaCrawlerCatalogLoader();
+    this.postgresSQLDatabaseConnector = new PostgreSQLDatabaseConnector();
+    this.sqLiteDatabaseConnector = new SQLiteDatabaseConnector();
   }
 }
