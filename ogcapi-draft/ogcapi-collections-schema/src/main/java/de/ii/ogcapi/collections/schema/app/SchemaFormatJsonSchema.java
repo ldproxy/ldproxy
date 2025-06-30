0c0ecbc9ae6ab0f8dd8578de688e48c68465d790
Noah <noah.vogt@gmail.com>
2025-06-30 16:21:59 +0200

    updated getMediaType and getContent for the JsonSchema

--- ogcapi-draft/ogcapi-collections-schema/src/main/java/de/ii/ogcapi/collections/schema/app/SchemaFormatJsonSchema.java
+++ ogcapi-draft/ogcapi-collections-schema/src/main/java/de/ii/ogcapi/collections/schema/app/SchemaFormatJsonSchema.java
@@ -7,44 +7,23 @@
  */
 package de.ii.ogcapi.collections.schema.app;
 
+import static de.ii.ogcapi.foundation.domain.ApiMediaType.JSON_SCHEMA_MEDIA_TYPE;
+
 import com.github.azahnen.dagger.annotations.AutoBind;
-import com.google.common.collect.ImmutableMap;
 import de.ii.ogcapi.collections.schema.domain.SchemaFormatExtension;
 import de.ii.ogcapi.features.core.domain.JsonSchemaDocument;
-import de.ii.ogcapi.features.core.domain.JsonSchemaDocument.VERSION;
 import de.ii.ogcapi.foundation.domain.ApiMediaType;
 import de.ii.ogcapi.foundation.domain.ApiMediaTypeContent;
 import de.ii.ogcapi.foundation.domain.ApiRequestContext;
-import de.ii.ogcapi.foundation.domain.ImmutableApiMediaType;
-import de.ii.ogcapi.foundation.domain.ImmutableApiMediaTypeContent;
 import de.ii.ogcapi.foundation.domain.Link;
 import de.ii.ogcapi.foundation.domain.OgcApi;
-import io.swagger.v3.oas.models.media.ObjectSchema;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.ws.rs.core.MediaType;
 
 @Singleton
 @AutoBind
 public class SchemaFormatJsonSchema implements SchemaFormatExtension {
-
-  static ApiMediaType JSON_SCHEMA_MEDIA_TYPE =
-      new ImmutableApiMediaType.Builder()
-          .type(
-              new MediaType(
-                  "application", "schema+json", ImmutableMap.of("schema", VERSION.V202012.url())))
-          .label("JSON Schema")
-          .parameter("json")
-          .fileExtension("schema.json")
-          .build();
-
-  static ApiMediaTypeContent SCHEMA_CONTENT =
-      new ImmutableApiMediaTypeContent.Builder()
-          .schema(new ObjectSchema())
-          .schemaRef("#/components/schemas/HTMLSchema")
-          .ogcApiMediaType(JSON_SCHEMA_MEDIA_TYPE)
-          .build();
 
   @Inject
   SchemaFormatJsonSchema() {}
