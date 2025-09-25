216f224d4d22f474aa4a78585c36dd2e43b7c2f8
Clemens Portele <portele@interactive-instruments.de>
2025-09-25 11:55:38 +0200

    move profile filtering to negotiation step

--- ogcapi-stable/ogcapi-features-core/src/main/java/de/ii/ogcapi/features/core/app/FeaturesCoreQueriesHandlerImpl.java
+++ ogcapi-stable/ogcapi-features-core/src/main/java/de/ii/ogcapi/features/core/app/FeaturesCoreQueriesHandlerImpl.java
@@ -31,7 +31,6 @@ import de.ii.ogcapi.foundation.domain.OgcApi;
 import de.ii.ogcapi.foundation.domain.OgcApiQueryParameter;
 import de.ii.ogcapi.foundation.domain.Profile;
 import de.ii.ogcapi.foundation.domain.ProfileExtension.ResourceType;
-import de.ii.ogcapi.foundation.domain.ProfileFilter;
 import de.ii.ogcapi.foundation.domain.ProfileSet;
 import de.ii.ogcapi.foundation.domain.QueriesHandler;
 import de.ii.ogcapi.foundation.domain.QueryHandler;
@@ -265,30 +264,15 @@ public class FeaturesCoreQueriesHandlerImpl extends AbstractVolatileComposed
 
     List<ProfileSet> allProfileSets = extensionRegistry.getExtensionsForType(ProfileSet.class);
 
-    List<Profile> givenProfiles =
-        allProfileSets.stream()
-            .filter(p -> p.isEnabledForApi(requestContext.getApi().getData(), collectionId))
-            .map(
-                profileSet ->
-                    profileSet
-                        .negotiateProfile(
-                            queryInput.getProfiles(),
-                            queryInput.getDefaultProfilesResource(),
-                            outputFormat,
-                            ResourceType.FEATURE,
-                            api.getData(),
-                            Optional.of(collectionId))
-                        .orElse(null))
-            .filter(Objects::nonNull)
-            .toList();
-
-    List<Profile> profiles = List.copyOf(givenProfiles);
-
-    for (Profile profile : givenProfiles) {
-      if (profile instanceof ProfileFilter) {
-        profiles = ((ProfileFilter) profile).filterProfiles(profiles);
-      }
-    }
+    List<Profile> profiles =
+        negotiateProfiles(
+            allProfileSets,
+            outputFormat,
+            ResourceType.FEATURE,
+            api.getData(),
+            Optional.of(collectionId),
+            queryInput.getProfiles(),
+            queryInput.getDefaultProfilesResource());
 
     Map<ApiMediaType, List<Profile>> alternateProfiles =
         getAlternateProfiles(
