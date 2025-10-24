aaa04f2090906e6fddf91754775f782e526fe6ac
p.zahnen <pascal.zahnen@gmx.de>
2025-10-24 17:13:22 +0200

    remove spatial components

--- ogcapi-stable/ogcapi-html/src/main/javascript/src/components/SortingEditor/Editor/index.jsx
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/SortingEditor/Editor/index.jsx
@@ -1,17 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 import { Row, Col, Collapse } from "reactstrap";
-import { MapSelect, roundBounds, boundsArraysEqual } from "./Spatial";
 
 import FieldFilter from "./Fields";
 
 const EditorBody = ({
   isOpen,
   fields,
-  backgroundUrl,
-  attribution,
-  spatial,
   filters,
   onAdd,
   deleteFilters,
@@ -20,31 +16,8 @@ const EditorBody = ({
   integerKeys,
   booleanProperty,
 }) => {
-  const [showMap, setShowMap] = useState(false);
-  const [bounds, setBounds] = useState(roundBounds(spatial));
-  const [mapFlag, setMapFlag] = useState(true);
-
-  const refreshMap = useCallback(() => setMapFlag((prev) => !prev), []);
-
-  const setBoundsIfNecessary = useCallback(
-    (newBounds, doRefreshMap) => {
-      const rounded = roundBounds(newBounds);
-
-      setBounds((prev) => {
-        if (boundsArraysEqual(prev, rounded)) {
-          return prev;
-        }
-        if (doRefreshMap) {
-          refreshMap();
-        }
-        return rounded;
-      });
-    },
-    [refreshMap]
-  );
-
   return (
-    <Collapse isOpen={isOpen} onEntered={() => setShowMap(true)}>
+    <Collapse isOpen={isOpen} onEntered={() => {}}>
       <Row>
         <Col md="6">
           {Object.keys(fields).length > 0 && (
@@ -68,17 +41,6 @@ const EditorBody = ({
             />
           )}
         </Col>
-        <Col md="6">
-          {showMap && spatial && spatial.length > 0 && (
-            <MapSelect
-              key={JSON.stringify(mapFlag)}
-              backgroundUrl={backgroundUrl}
-              attribution={attribution}
-              bounds={bounds}
-              onChange={setBoundsIfNecessary}
-            />
-          )}
-        </Col>
       </Row>
     </Collapse>
   );
@@ -89,9 +51,6 @@ EditorBody.displayName = "EditorBody";
 EditorBody.propTypes = {
   isOpen: PropTypes.bool,
   fields: PropTypes.objectOf(PropTypes.string),
-  backgroundUrl: PropTypes.string.isRequired,
-  attribution: PropTypes.string.isRequired,
-  spatial: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)),
   // eslint-disable-next-line react/forbid-prop-types
   filters: PropTypes.object.isRequired,
   onAdd: PropTypes.func,
@@ -106,7 +65,6 @@ EditorBody.propTypes = {
 EditorBody.defaultProps = {
   isOpen: false,
   fields: {},
-  spatial: null,
   onAdd: () => {},
 };
 
--- ogcapi-stable/ogcapi-html/src/main/javascript/src/components/SortingEditor/util.js
+++ ogcapi-stable/ogcapi-html/src/main/javascript/src/components/SortingEditor/util.js
@@ -1,5 +1,3 @@
-import moment from "moment";
-
 export const getBaseUrl = () => {
   let baseUrl = new URL(window.location.href);
   if (process.env.NODE_ENV !== "production") {
@@ -18,7 +16,10 @@ export const extractFields = (obj) => {
   if (obj && obj.properties) {
     // eslint-disable-next-line
     for (const key in obj.properties) {
-      if (obj.properties[key]["x-ogc-role"] && obj.properties[key]["x-ogc-role"].startsWith("primary-")) {
+      if (
+        obj.properties[key]["x-ogc-role"] &&
+        obj.properties[key]["x-ogc-role"].startsWith("primary-")
+      ) {
         // eslint-disable-next-line no-continue
         continue;
       }
@@ -38,55 +39,4 @@ export const extractFields = (obj) => {
   }
 
   return { fields, code, integerKeys, booleanProperty };
-};
-
-export const extractInterval = (obj) => {
-  let start = null;
-  let end = null;
-  let temporal = {};
-
-  const parseTemporalExtent = (temporalExtent) => {
-    const starting = temporalExtent.interval[0][0];
-    const ending = temporalExtent.interval[0][1];
-    const startingUnix = moment.utc(starting).valueOf();
-    const endingUnix = ending ? moment.utc(ending).valueOf() : moment.utc().valueOf();
-    return { start: startingUnix, end: endingUnix };
-  };
-
-  if (obj && obj.extent) {
-    const { temporal: temporalExtent } = obj.extent;
-
-    if (temporalExtent) {
-      temporal = parseTemporalExtent(temporalExtent);
-    }
-
-    start = temporal.start;
-    end = temporal.end;
-  }
-
-  return { start, end, temporal };
-};
-
-export const extractSpatial = (obj) => {
-  let spatial = [];
-
-  if (obj && obj.extent) {
-    const { spatial: spatialExtent } = obj.extent;
-
-    if (spatialExtent) {
-      const bounds = spatialExtent.bbox;
-      const transformedBounds =
-        bounds[0].length === 6
-          ? bounds.map((innerArray) => [
-              [innerArray[0], innerArray[1]],
-              [innerArray[3], innerArray[4]],
-            ])
-          : bounds.map((innerArray) => [
-              [innerArray[0], innerArray[1]],
-              [innerArray[2], innerArray[3]],
-            ]);
-      spatial = transformedBounds.flat();
-    }
-  }
-  return { spatial };
 };
