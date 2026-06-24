d3896b2669b1ca7fabac88e1c390dcc17b993c25
Afeef Neiroukh <aneiroukh@interactive-instruments.de>
2026-06-24 16:24:30 +0200

    processes endpoint: pagination support for HTML

--- ogcapi-draft/ogcapi-processes/src/main/java/de/ii/ogcapi/processes/app/html/ProcessDescriptionsFormatHtml.java
+++ ogcapi-draft/ogcapi-processes/src/main/java/de/ii/ogcapi/processes/app/html/ProcessDescriptionsFormatHtml.java
@@ -16,15 +16,22 @@ import de.ii.ogcapi.foundation.domain.FormatExtension;
 import de.ii.ogcapi.foundation.domain.I18n;
 import de.ii.ogcapi.foundation.domain.OgcApi;
 import de.ii.ogcapi.foundation.domain.OgcApiDataV2;
+import de.ii.ogcapi.foundation.domain.OgcApiQueryParameter;
+import de.ii.ogcapi.foundation.domain.QueryParameterSet;
 import de.ii.ogcapi.html.domain.FormatHtml;
 import de.ii.ogcapi.html.domain.HtmlConfiguration;
 import de.ii.ogcapi.html.domain.NavigationDTO;
+import de.ii.ogcapi.processes.app.QueryParameterLimitProcesses;
+import de.ii.ogcapi.processes.app.QueryParameterOffsetProcesses;
 import de.ii.ogcapi.processes.domain.ProcessDescriptionsFormatExtension;
 import de.ii.ogcapi.processes.domain.ProcessDescriptionsRepresentation;
+import de.ii.ogcapi.processes.domain.model.ProcessDescriptionRepository;
 import de.ii.xtraplatform.web.domain.URICustomizer;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @title HTML
@@ -35,10 +42,13 @@ public class ProcessDescriptionsFormatHtml
     implements ProcessDescriptionsFormatExtension, FormatHtml {
 
   private final I18n i18n;
+  // ToDo Repository access in Format file seems like bad smell, move pagnitation logic?
+  private final ProcessDescriptionRepository repository;
 
   @Inject
-  public ProcessDescriptionsFormatHtml(I18n i18n) {
+  public ProcessDescriptionsFormatHtml(I18n i18n, ProcessDescriptionRepository repository) {
     this.i18n = i18n;
+    this.repository = repository;
   }
 
   @Override
@@ -88,6 +98,60 @@ public class ProcessDescriptionsFormatHtml
 
     HtmlConfiguration htmlConfig = api.getData().getExtension(HtmlConfiguration.class).orElse(null);
 
+    QueryParameterSet queryParameterSet = requestContext.getQueryParameterSet();
+    Optional<Integer> optionalLimit = Optional.empty();
+    Optional<Integer> optionalOffset = Optional.empty();
+    for (OgcApiQueryParameter queryParameter : queryParameterSet.getDefinitions()) {
+      if (queryParameter instanceof QueryParameterLimitProcesses) {
+        optionalLimit = ((QueryParameterLimitProcesses) queryParameter).parse(queryParameterSet);
+      }
+      if (queryParameter instanceof QueryParameterOffsetProcesses) {
+        optionalOffset = ((QueryParameterOffsetProcesses) queryParameter).parse(queryParameterSet);
+      }
+    }
+
+    // We just .get() because of the default values
+    int offset = optionalOffset.get();
+    int limit = optionalLimit.get();
+    int size = repository.getAll().size();
+    int page = Math.floorDiv(offset, limit) + 1;
+    int pages = (int) Math.ceil((double) size / limit);
+
+    ImmutableList.Builder<NavigationDTO> pagination = new ImmutableList.Builder<>();
+
+    if (page > 1) {
+      pagination
+          .add(new NavigationDTO("«", String.format("limit=%d&offset=%d", limit, 0)))
+          .add(new NavigationDTO("‹", String.format("limit=%d&offset=%d", limit, offset - limit)));
+    } else {
+      pagination.add(new NavigationDTO("«")).add(new NavigationDTO("‹"));
+    }
+
+    long from = Math.max(1, page - 2);
+    long to = Math.min(pages, from + 4);
+    if (to == pages) {
+      from = Math.max(1, to - 4);
+    }
+    for (long i = from; i <= to; i++) {
+      if (i == page) {
+        pagination.add(new NavigationDTO(String.valueOf(i), true));
+      } else {
+        pagination.add(
+            new NavigationDTO(
+                String.valueOf(i), String.format("limit=%d&offset=%d", limit, (i - 1) * limit)));
+      }
+    }
+
+    if (page < pages) {
+      pagination
+          .add(new NavigationDTO("›", String.format("limit=%d&offset=%d", limit, offset + limit)))
+          .add(
+              new NavigationDTO(
+                  "»", String.format("limit=%d&offset=%d", limit, (pages - 1) * limit)));
+    } else {
+      pagination.add(new NavigationDTO("›")).add(new NavigationDTO("»"));
+    }
+
     return ImmutableProcessDescriptionsView.builder()
         .apiData(api.getData())
         .basePath(requestContext.getBasePath())
@@ -97,12 +161,14 @@ public class ProcessDescriptionsFormatHtml
         .htmlConfig(htmlConfig)
         .noIndex(isNoIndexEnabledForApi(api.getData()))
         .uriCustomizer(requestContext.getUriCustomizer().copy())
+        .uri(URI.create("processes"))
         .i18n(i18n)
         .language(requestContext.getLanguage().orElse(null))
         .description(i18n.get("processDescriptionsLink", requestContext.getLanguage()))
         .title(i18n.get("processDescriptionsTitle", requestContext.getLanguage()))
         .rawLinks(processDescriptionsRepresentation.getLinks())
         .user(requestContext.getUser())
+        .pagination(pagination.build())
         .build();
   }
 }
--- ogcapi-draft/ogcapi-processes/src/main/java/de/ii/ogcapi/processes/app/html/ProcessDescriptionsView.java
+++ ogcapi-draft/ogcapi-processes/src/main/java/de/ii/ogcapi/processes/app/html/ProcessDescriptionsView.java
@@ -7,13 +7,22 @@
  */
 package de.ii.ogcapi.processes.app.html;
 
+import com.google.common.base.Splitter;
 import de.ii.ogcapi.foundation.domain.I18n;
+import de.ii.ogcapi.html.domain.NavigationDTO;
 import de.ii.ogcapi.html.domain.OgcApiView;
 import de.ii.ogcapi.processes.domain.ProcessDescriptionRepresentation;
 import de.ii.xtraplatform.web.domain.URICustomizer;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -23,6 +32,47 @@ public abstract class ProcessDescriptionsView extends OgcApiView {
   }
 
   public abstract List<ProcessDescriptionRepresentation> processDescriptions();
+
+  @Nullable
+  public abstract List<NavigationDTO> pagination();
+
+  @Nullable
+  public abstract URI uri();
+
+  @Value.Derived
+  public String getPath() {
+    String path = uri().getPath();
+    return path;
+  }
+
+  @Value.Derived
+  public Function<String, String> getQueryWithout() {
+    return without -> {
+      List<String> ignore = Splitter.on(',').trimResults().omitEmptyStrings().splitToList(without);
+
+      try {
+        List<NameValuePair> query =
+            new URIBuilder(RawQuery())
+                .getQueryParams().stream()
+                    .filter(kvp -> !ignore.contains(kvp.getName().toLowerCase()))
+                    .collect(Collectors.toList());
+
+        if (query.isEmpty()) {
+          return "?";
+        }
+        return '?' + new URIBuilder().setParameters(query).build().getRawQuery() + '&';
+      } catch (URISyntaxException e) {
+        throw new IllegalStateException(
+            String.format("Failed to parse query parameters: '%s'", RawQuery()), e);
+      }
+    };
+  }
+
+  @Value.Derived
+  public String RawQuery() {
+
+    return "?" + (uri().getRawQuery() != null ? uri().getRawQuery() + "&" : "");
+  }
 
   @Value.Derived
   public String none() {
